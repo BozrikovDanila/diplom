7592e17dce087a35041158038c6a04364f44c1d5
BozrikovDanila <bozrikov03@mail.ru>
2024-05-01 23:04:41 +0300

    миграции на все сущности без зависимых атрибутов

---
+++ database/migrations/2024_05_01_195750_create_clients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 100);
+            $table->string('second_name', 100);
+            $table->string('last_name', 100);
+            $table->string('email');
+            $table->string('phone');
+            $table->integer('employees_number')->comment('Кол-во сотрудников компании');
+            $table->integer('assessments_number')->comment('Кол-во оценок в подписке');
+            $table->timestamp('duration')->comment('Длительность подписки');
+            $table->boolean('instant_access')->comment('Выдать доступ сразу или после оплаты');
+            $table->string('INN', 12)->comment('ИНН');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
